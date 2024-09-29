27f39d59510a1d319f75634aee75d22fb7803973
Shafin <166398150+shafin-ahmed-8122003@users.noreply.github.com>
2024-09-29 07:11:40 +0000

    Updated main.py

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-print("By the name of Allah!")
+import os
+import time
+
+def clear_screen():
+    os.system("clear")
+
+def wait_for(seconds):
+    time.sleep(seconds)
+
+while True:
+    clear_screen()
+
+    print("Welcome to BudgetBuddy!")
+    print()
+
+    print("1. Add an expanse")
+    print("2. Remove an expanse")
+    
+
+    print()
+    print("3. Exit")
+    print()
+
+    choice = input("> ")
+
+    
+
+    if (choice == "3"):
+        clear_screen()
+        print("Thanks for using BudgetBuddy!!")
+        wait_for(1)
+        clear_screen()
+        break
